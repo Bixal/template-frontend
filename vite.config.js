2f2052bb85c2c9a5701b4113c1220a4c0f131f8c
James Mejia <james.mejia@bixal.com>
2025-10-24 09:27:00 -0500

    Use modern sass api for faster compiles

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        api: "modern-compiler",
         quietDeps: true,
         loadPaths: ["node_modules/@uswds/uswds/packages"],
       },
